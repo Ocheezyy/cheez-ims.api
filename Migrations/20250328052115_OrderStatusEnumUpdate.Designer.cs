ffae60d755a84fcdc700e114114ee942e3574747
ocheezyyy <seanodonnellse@gmail.com>
2025-03-28 01:21:38 -0400

    migrations update

--- Migrations/20250328052115_OrderStatusEnumUpdate.Designer.cs
+++ Migrations/20250328052115_OrderStatusEnumUpdate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using cheez_ims_api.Data;
@@ -12,9 +13,11 @@ using cheez_ims_api.models;
 namespace cheez_ims_api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250328052115_OrderStatusEnumUpdate")]
+    partial class OrderStatusEnumUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
